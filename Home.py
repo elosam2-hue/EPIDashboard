c7577f9b813e88b687100c3385b81936a3095044
Elias <elosam2@gmail.com>
2026-06-07 00:26:55 +0300

    Update header text in Home.py

--- Home.py
+++ Home.py
@@ -171,8 +171,8 @@ col1, col2, col3 = st.columns([1,4,1])
 with col1:
     st.image("assets/moh_logo.png", width=100)
 with col2:
-st.markdown('<div class="main-header-container"><h3 style="text-align: center; margin: 0;">South Ethiopia Immunization Data Triangulation Application</h3></div>', unsafe_allow_html=True)
-    st.markdown('<p class="subtitle">Comprehensive analysis of vaccine administration and distribution data</p>', unsafe_allow_html=True)
+st.markdown('<div class="main-header-container"><h1>Immunization Data Triangulation Application</h1></div>', unsafe_allow_html=True)
+st.markdown('<p class="subtitle">Comprehensive analysis of vaccine administration and distribution data</p>', unsafe_allow_html=True)
 with col3:
     st.image("assets/eth_flag.png", width=100)
 
