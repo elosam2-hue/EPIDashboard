2f39fbd70075db65efe35ceca72c9a7801f032a7
Elias <elosam2@gmail.com>
2026-06-11 11:19:37 +0300

    Update Dashboard1.py

--- pages/Dashboard1.py
+++ pages/Dashboard1.py
@@ -486,7 +486,7 @@ def main():
     periods = sorted(df_all["Period"].unique())
     selected_period = st.sidebar.selectbox("Select Period", ["All"] + periods)
 
-# Year filter
+    # Year filter
     year = sorted(df_all["year"].unique())
     selected_year = st.sidebar.selectbox("Select year", ["All"] + year)
 
@@ -504,7 +504,7 @@ def main():
         filtered_df = filtered_df[filtered_df["Woreda_Admin"] == selected_woreda]
     if selected_period != "All":
         filtered_df = filtered_df[filtered_df["Period"] == selected_period]
-if selected_year != "All":
+    if selected_year != "All":
         filtered_df = filtered_df[filtered_df["year"] == selected_year]
 
     
